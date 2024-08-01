c5ce03c25d594978f843b45d3770f685ea683fe6
vishal-gopalakrishnan <vishal.gopalakrishnan@utexas.edu>
2024-08-01 15:01:18 -0500

    Added files that can move robot in gazebo

---
+++ kinova_control/src/move_robot_semicircle.py
@@ -0,0 +1,9 @@
+#! /usr/bin/env python3
+import os
+with open("/home/autognc-admin/catkin_ws/src/kinova-ros/kinova_control/src/move_robot_semicircle_bot.py") as file:
+	exec(file.read())
+with open("/home/autognc-admin/catkin_ws/src/kinova-ros/kinova_control/src/move_robot_retract_semicircle_mid.py") as file:
+	exec(file.read())
+with open("/home/autognc-admin/catkin_ws/src/kinova-ros/kinova_control/src/move_robot_semicircle_top.py") as file:
+	exec(file.read())
+	
